e61da48e3b425bb81a548c362d569ac893004d86
ELIJAHKUNGU <elijahkungu100@gmail.com>
2022-05-07 20:10:32 +0300

    google-auth

--- stories/app.js
+++ stories/app.js
@@ -1,14 +1,25 @@
-const path = require('path');
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const morgan = require('morgan');
-const exphbs = require('express-handlebars');
+const path = require('path')
+const express = require('express')
+    //const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+    //const methodOverride = require('method-override')
+const passport = require('passport')
+const session = require('express-session')
+    //const MongoStore = require('connect-mongo');
+const connectDB = require('./config/db')
 
 
 
 //load config
 dotenv.config({ path: './config/config.env' });
+require('./config/passport')(passport)
+
+
+//CALL DB
+connectDB();
+
 const app = express();
 
 if (process.env.NODE_ENV === 'development') {
@@ -17,11 +28,22 @@ if (process.env.NODE_ENV === 'development') {
 //set public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-//CALL DB
-connectDB();
+
 // Handlebars
 app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main" }));
 app.set('view engine', '.hbs');
+
+//SESSIONS
+app.use(session({
+    secret: 'Hello World',
+    resave: false,
+    saveUninitialized: false
+}))
+
+
+//PASSPORT MIDDLEWARES
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use('/', require('./routes/index'));
 
