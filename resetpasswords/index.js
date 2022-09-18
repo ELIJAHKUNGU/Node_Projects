04399b4324fc1dec599f34aa51e7e738349b3cff
elijah <elijahkungu100@gmail.com>
2022-09-18 20:04:23 +0300

    controllers

--- resetpasswords/index.js
+++ resetpasswords/index.js
@@ -1,26 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-require("express-async-errors")
-require("dotenv").config()
-const DbConnection = require("./db")
+require("express-async-errors");
+require("dotenv").config();
 
-const app = express()
-
-
+const express = require("express");
+const app = express();
+const connection = require("./db");
+const cors = require("cors");
 const port = 8080;
-
-DbConnection();
-
+const mainRoute  = require("./route/index.route")
+const DbConnection = require("./db")
+DbConnection()
 
 app.use(cors());
 app.use(express.json());
-// app.use("/api/v1", require("./routes/index.route"));
+
+// API routes
+app.use("/api/v1", mainRoute);
+
 app.use((error, req, res, next) => {
   res.status(500).json({ error: error.message });
 });
 
+app.listen(port, () => {
+  console.log("Listening to Port ", port);
+});
 
-
-app.listen(5000, ()=> {
-    console.log("The server is up and running");
-})
+module.exports = app;
